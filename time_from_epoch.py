bb49f3f64624124ca11773ed14a95fc8a0f72936
kamScripts <k.gebski1@gmail.com>
2025-05-02 10:36:37 +0100

    calculate time

--- time_from_epoch.py
+++ time_from_epoch.py
@@ -1,18 +1,33 @@
 import time
+import datetime
+# seconds from epoch (1-1-1970 UTC) Uk summer time = +1h.
+t=int(time.time())
 
-t=time.time()
-print(t)
+
+#Convert time into seconds
 
 SECONDS_IN_MINUTES = 60
 SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTES
 SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
-def time_from_epoch
-print(SECONDS_IN_MINUTES, SECONDS_IN_HOUR, SECONDS_IN_DAY)
 
-days_since_epoch_one = int(t // SECONDS_IN_DAY)
+print('localtime', time.localtime())
+def time_since_epoch(seconds_from_epoch):
+    """Calculate days from epoch (1-1-1970UTC)"""
+    days_since_epoch = seconds_from_epoch // SECONDS_IN_DAY
+    print('days since epoch one (1-1-1970 UTC): ', days_since_epoch)
+    
 
-print('days since epoch one (1-1-1970): ', days_since_epoch_one)
+#Remaining seconds in the day
+def current_time_since_epoch(seconds_from_epoch, summerTime=True):
+    
+    reminder_from_days = seconds_from_epoch % SECONDS_IN_DAY
+    hours = reminder_from_days // SECONDS_IN_HOUR
+    if summerTime:
+        hours+=1
+    minutes = (reminder_from_days % SECONDS_IN_HOUR) // SECONDS_IN_MINUTES
+    seconds = reminder_from_days % SECONDS_IN_MINUTES
+    print(f'{hours}:{minutes}:{seconds}')
 
-reminder_from_days = t % SECONDS_IN_DAY
 
-hours = reminder_from_days // 
+time_since_epoch(t)
+current_time_since_epoch(t)
