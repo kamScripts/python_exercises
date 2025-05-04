ab06b898b431d997fc1d647ff86333376a1e6c8b
kamScripts <k.gebski1@gmail.com>
2025-05-04 18:15:47 +0100

    cypher

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,8 +1,6 @@
-
 def caesar_cypher(string, cypher):
     encrypted = ''
     for c in string:
-        print((ord(c)+cypher))
-    return encrypted   
+        encrypted += chr(ord(c) + cypher)
+    return encrypted
 print(caesar_cypher('HAL', -1))
-
