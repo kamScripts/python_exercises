2c964334e457a441d458cbc2169729e4c9ab8480
kamScripts <k.gebski1@gmail.com>
2025-05-04 22:09:15 +0100

    percentage of words in the list

--- reading_words.py
+++ reading_words.py
@@ -1,3 +1,4 @@
+import math
 def more_than_20_chars(word:str)->None:
     """Find and print strings with more than 20 chars.
         
@@ -8,7 +9,7 @@ def more_than_20_chars(word:str)->None:
     word = word.strip()
     if len(word) > 20:
         return word
-        
+
 def has_no_e(word:str)->bool:
     """Check if a word contains letter 'e'.
     
@@ -17,10 +18,23 @@ def has_no_e(word:str)->bool:
         Returns: True if string does not contain 'e'
     """
     return word.find('e')==-1
-
-with open('words.txt', 'r', encoding='UTF-8') as fin:
-    #more_than_20_chars(fin)
-    for line in fin:
-        stripped= line.strip()
-        if has_no_e(stripped):
-            print(stripped)
+def check_percentage(file:str, func)->float:
+    """Measures how many times words meeting condition occur on the list.
+    
+    file: list - text file
+    func: function - condition to check
+    
+    Returns: float - Percentage of words in the list that meet condition.
+    """
+    with open(file, 'r', encoding='UTF-8') as fin:
+        #more_than_20_chars(fin)
+        total = 0
+        counter = 0
+        for line in fin:
+            total+=1
+            stripped= line.strip()
+            if func(stripped):
+                counter+=1
+        return round(counter/total * 100, 4)
+res = check_percentage('words.txt', has_no_e)
+print(res, '%')
