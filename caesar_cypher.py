1f9855f2bb216dcd2f9c1c08da609c65e1d059f2
kamScripts <k.gebski1@gmail.com>
2025-05-04 18:52:08 +0100

    add rotate_letter to rotate only alphabet

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,6 +1,18 @@
-def caesar_cypher(string, cypher):
+def rotate_letter(l, n):
+    """Rotate letter characters by n spaces"""
+    if l.isupper():
+        start=ord('A')
+    elif l.islower():
+        start=ord('a')
+    else:
+        return l
+    char=ord(l) - start
+    i = (char + n) % 26 + start
+    return chr(i)
+def caesar_cypher(string, n):
+    """Rotate string by n spaces"""
     encrypted = ''
     for c in string:
-        encrypted += chr(ord(c) + cypher)
+        encrypted += rotate_letter(c, n)
     return encrypted
-print(caesar_cypher('HAL', -1))
+print(caesar_cypher("""melon""", 13))
