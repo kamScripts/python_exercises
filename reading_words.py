6b48c52bd7e1c7d7358a9cf594b837585dee1f18
kamScripts <k.gebski1@gmail.com>
2025-05-04 21:24:41 +0100

    has_no_e func + generalize funcs

--- reading_words.py
+++ reading_words.py
@@ -1,8 +1,26 @@
-def more_than_20_chars(file):
-    with open(file, mode="r", encoding='UTF-8') as fin:
-        line= fin.readline()
-        for line in fin:
-            word = line.strip()
-            if len(word) > 20:
-                print(word)
-more_than_20_chars('words.txt')
+def more_than_20_chars(word:str)->None:
+    """Find and print strings with more than 20 chars.
+        
+        word - string
+        
+        returns: word longer than 20 characters.
+    """
+    word = word.strip()
+    if len(word) > 20:
+        return word
+        
+def has_no_e(word:str)->bool:
+    """Check if a word contains letter 'e'.
+    
+        word: string 
+               
+        Returns: True if string does not contain 'e'
+    """
+    return word.find('e')==-1
+
+with open('words.txt', 'r', encoding='UTF-8') as fin:
+    #more_than_20_chars(fin)
+    for line in fin:
+        stripped= line.strip()
+        if has_no_e(stripped):
+            print(stripped)
