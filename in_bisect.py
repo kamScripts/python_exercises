6fbf2a8c27c2eebd3d085c8d452ad9c5cd4d01a1
kamScripts <k.gebski1@gmail.com>
2025-05-08 17:12:37 +0100

    compare b-search time to iteration search

--- in_bisect.py
+++ in_bisect.py
@@ -1,3 +1,6 @@
+import time
+from wordlist import word_list as append_to_list
+
 def in_bisect(target_value,sorted_list):
     """Binary search to check if a element is on the list
     
@@ -5,23 +8,26 @@ def in_bisect(target_value,sorted_list):
     sorted_list: list of sorted elements
     
     Returns: boolean"""
-    
+
     l=0
     r=len(sorted_list)-1
-
-
-
     while l <=r:
         m= l+((r-l)//2)
         if target_value== sorted_list[m]:
             return True
         if sorted_list[m] < target_value:
             l = m+1
-            print(l,r)
+    
         if sorted_list[m] > target_value:
-
             r= m-1
     return False
+t= append_to_list('words.txt')
 
+s1=time.time()
+print(in_bisect('wryly', t))
+e1=time.time()
+s2=time.time()
+print('wryly' in t)
+e2=time.time()
+print(f'binary search time: {e1-s1}s, iteration search: {e2-s2}s')
 
-print(in_bisect(10, [1,2,3,4,5,6]))
--- wordlist.py
+++ wordlist.py
@@ -11,10 +11,4 @@ def word_list2(file):
         for line in fin:
             t= t + [line.strip()]
     return t
-start1=time.time()
-words=word_list('words.txt')
-end1=time.time()
-s2=time.time()
-words_2= word_list2('words.txt')
-e2=time.time()
-print(f'append method {end1-start1}, el: {len(words)}, concatenation {e2-s2}, el: {len(words_2)}')
+
