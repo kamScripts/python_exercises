6e14419cb65773461e3246421ebeb7f1d059d881
kamScripts <k.gebski1@gmail.com>
2025-05-08 12:35:39 +0100

    middle, chop and is_anagram

--- lists.py
+++ lists.py
@@ -6,14 +6,15 @@ def nested_sum(t):
     Returns: Integer, sum of nested lists.
     """
     total=0
-    for i in range(len(t)):
-        total+=sum(t[i])
+    for i in t:
+        total+=sum(i)
     return total
 a=[[1,2], [3], [4,5,6]]
 print(nested_sum(a))
 
 def cumsum(t):
-    """Cumulative sum, add previous elements of the list
+    """Cumulative sum, each elements is a sum of element
+    and previous elements
     
     t: list of numbers
     
@@ -25,4 +26,52 @@ def cumsum(t):
     for i in range(len(t)):
         new_list.append(sum(t[:i+1]))
     return new_list
-print(cumsum([1,2,3,4,5]))
+#print(cumsum([1,2,3,4,5]))
+
+def middle(t):
+    """Removes first and last elements of the list
+    
+    t: list
+    
+    Returns: New list.
+    """
+    return t[1:-1]
+
+#print(middle([1,2,3,4]))
+def chop(t):
+    """Modifies list by removing first and last element from the list.
+    
+    t: list
+    
+    Returns: None"""
+    
+    del t[0]
+    del t[-1]
+b=[1,2,3]
+#chop(b)
+#print(b)
+
+def is_sorted(t):
+    """Checks if list is sorted
+    
+    t: list
+    
+    Returns: Boolean"""
+    return t==sorted(t)
+#print(is_sorted([3,2]))
+
+def is_anagram(word1, word2):
+    """Checks if words are anagrams"""
+    t= list(word1)
+    x= list(word2)
+    t.sort()
+    x.sort()
+    print(t,x)
+    if len(t) != len(x):
+        return False
+    for i, l in enumerate(x):
+
+        if t[i] != l:
+            return False
+    return True
+print(is_anagram('ab1ca', 'bc1aa'))
