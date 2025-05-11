ab65ec2b1946851fd3ca5eb7af8db3bb1c59b899
kamScripts <k.gebski1@gmail.com>
2025-05-11 20:47:46 +0100

    letters frequency output func

--- tuple_most_frequent.py
+++ tuple_most_frequent.py
@@ -5,10 +5,10 @@ def count_letters(text):
         for line in fin:
             for c in line.lower().strip():
                 if c>='a':
-                    
+                    count+=1
                     val = d.get(c, 0)
                     d[c] = val+1
-    return (d,c)
+    return (d,count)
 pl=count_letters('sample_pl.txt')
 eng=count_letters('sample_eng.txt')
 de=count_letters('sample_de.txt')
@@ -18,12 +18,27 @@ de=count_letters('sample_de.txt')
 
 def most_frequent(d):
     m_fr=[]
-    for key, val in d.items():
+    count=d[1]
+    for key, val in d[0].items():
 
-        m_fr.append((val, key))
+        m_fr.append((val,f'{round(val/count*100,2)}%', key))
     m_fr.sort()
     return m_fr[::-1]
 
-print(most_frequent(pl[0]))
-print(most_frequent(eng[0]))
-print(most_frequent(de[0]))
+def print_output(t, lang):
+    headers=('letters', 'frequency', 'counted')
+    print('\n')
+    print(lang.center(35, '*'), '\n')
+    print('+', end=' ')
+    print(': | '.join(headers), end=' ')
+    print('+')
+    print('|----------+------------+---------|')
+    for c in t:
+        print('| ',c[2].ljust(8), end='')
+        print('|',c[1].ljust(11), end='')
+        print('|',f'{c[0]}'.ljust(8), end='|')
+        print()
+    print('+----------+------------+---------+')
+    
+most_pl=most_frequent(pl)   
+print_output(most_pl, 'Polish')
