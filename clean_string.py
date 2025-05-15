504de648e3478283c561b5490cad7f8417d16e19
kamScripts <k.gebski1@gmail.com>
2025-05-15 16:25:23 +0100

    add word count

--- clean_string.py
+++ clean_string.py
@@ -1,25 +1,36 @@
 import string
 
-p= string.punctuation
+p= string.punctuation+'“”‘’'
 w= string.whitespace
 empty_chars=['' for _ in range(len(p))]
+w_e_c =[' ' for _ in range(len(w))]
 tr= dict(zip(p, empty_chars))
 mytable=str.maketrans(tr)
-
 
 def clean_string(file):
     """Break each line of text to single words and strip
     whitespaces and punctuation
-    Returns: dict
+    Returns: tuple (dict, total_words, unique_words)
     """
     with open(file, 'r', encoding='UTF-8') as fin:
         d={}
+        unique=0
+        total=0
+        fin.seek(52)
+        print(fin.readline())
         for line in fin:
-            l= line.translate(mytable).strip().lower().split(' ')
+            line=line.translate(mytable)
+            l= line.strip().lower().split(' ')
             for word in l:
+                if word in w or word.isdigit():
+                    continue
                 if word not in d:
-                    d[word] = 1
+                    unique+=1
+                    total+=1
+                    d[word.strip('—')] = 1
                 else:
+                    total+=1
                     d[word]+=1
-        return d
-print(clean_string('Alice_Adeventures_in_wonderlad.txt'))
+        return d, total, unique
+words, t, u=clean_string('Alice_Adeventures_in_wonderlad.txt')
+print(words, t, u)
